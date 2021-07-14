da4428b4411ecd06a79689eb3de8d40ca3d93b0a
Merve Özçifçi <merveozcifci7@gmail.com>
2021-07-14 10:59:11 +0300

    removed animation

--- assets/js/site.js
+++ assets/js/site.js
@@ -53,8 +53,6 @@ var PrimeFlex = {
     },
 
     onToggleMobileMenu: function (e) {
-        var menubutton = e.currentTarget;
-
         if (this.hasClass(this.menuWrapper, 'active')) {
             this.removeClass(this.menuWrapper, 'active');
             this.removeClass(this.mask, 'layout-mask-active');
@@ -65,48 +63,6 @@ var PrimeFlex = {
     },
 
     copyText: function (e) {
-        let confettiAmount = 60,
-            confettiColors = [
-                '#7d32f5',
-                '#f6e434',
-                '#63fdf1',
-                '#e672da',
-                '#295dfe',
-                '#6e57ff'
-            ],
-            random = (min, max) => {
-                return Math.floor(Math.random() * (max - min + 1) + min);
-            },
-            createConfetti = to => {
-                let elem = document.createElement('i'),
-                    set = Math.random() < 0.5 ? -1 : 1;
-                elem.style.setProperty('--x', random(-260, 260) + 'px');
-                elem.style.setProperty('--y', random(-160, 160) + 'px');
-                elem.style.setProperty('--r', random(0, 360) + 'deg');
-                elem.style.setProperty('--s', .1);
-                elem.style.setProperty('--b', confettiColors[random(0, 5)]);
-                to.appendChild(elem);
-            };
-
-        if (!this.copyButton.classList.contains('animation')) {
-            this.copyButton.classList.add('animation');
-            for (let i = 0; i < confettiAmount; i++) {
-                createConfetti(this.copyButton);
-            }
-            setTimeout(() => {
-                this.copyButton.classList.add('confetti');
-                setTimeout(() => {
-                    this.copyButton.querySelectorAll('i').forEach(i => i.remove());
-                }, 500);
-            }, 50);
-        } else {
-            this.copyButton.classList.remove('animation', 'confetti');
-        }
-
-        setTimeout(() => {
-            this.copyButton.classList.remove('animation', 'confetti');
-        }, 650);
-
         var copyText = e.currentTarget.previousElementSibling;
         copyText.select();
         copyText.setSelectionRange(0, 99999);
