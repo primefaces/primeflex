91b6c3cdf38f5e5c80eeed31ca61d310d34c88a4
Cagatay Civici <cagatay.civici@gmail.com>
2023-05-21 12:52:28 +0300

    Add SEO tags

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -54,10 +54,22 @@ export default function Layout(props) {
     return (
         <div className={wrapperClassName}>
             <Head>
-                <title>PrimeFlex</title>
+                <title>PrimeFlex - Utility-First CSS Library</title>
                 <meta charSet="UTF-8" />
-                <meta name="description" content="PrimeFlex is a lightweight responsive CSS utility library to accompany Prime UI libraries and static webpages as well." />
-                <link rel="icon" href="https://primefaces.org/cdn/primereact/images/favicon.ico" type="image/x-icon"></link>
+                <meta name="description" content="PrimeFlex is a utility-first CSS library." />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@primeflexcss" />
+                <meta name="twitter:title" content="PrimeFlex | Utility-First CSS Library" />
+                <meta name="twitter:description" content="PrimeFlex is a utility-first CSS library." />
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:title" content="PrimeFlex - Utility-First CSS Library"></meta>
+                <meta property="og:url" content="https://primeflex.org"></meta>
+                <meta property="og:description" content="PrimeFlex is a utility-first CSS library." />
+                <meta property="og:image" content="https://www.primefaces.org/static/social/primereact-preview.jpg"></meta>
+                <meta property="og:ttl" content="604800"></meta>
+                <link rel="icon" href="https://primefaces.org/cdn/primereact/images/favicon.ico" type="image/x-icon"></link>                
             </Head>
             <Topbar dark={props.dark} onMenuButtonClick={onMenuButtonClick} onToggleColorScheme={toggleColorScheme} />
             <Menu active={sidebarActive} darkTheme={props.dark} />
--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,22 @@ export default function Home(props) {
     return (
         <div className={rootClassName}>
             <Head>
+                <title>PrimeFlex - Utility-First CSS Library</title>
                 <meta charSet="UTF-8" />
-                <title>PrimeFlex</title>
-                <meta name="description" content="PrimeFlex is a lightweight responsive CSS utility library to accompany Prime UI libraries and static webpages as well." />
+                <meta name="description" content="PrimeFlex is a utility-first CSS library." />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@primeflexcss" />
+                <meta name="twitter:title" content="PrimeFlex | Utility-First CSS Library" />
+                <meta name="twitter:description" content="PrimeFlex is a utility-first CSS library." />
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:title" content="PrimeFlex - Utility-First CSS Library"></meta>
+                <meta property="og:url" content="https://primeflex.org"></meta>
+                <meta property="og:description" content="PrimeFlex is a utility-first CSS library." />
+                <meta property="og:image" content="https://www.primefaces.org/static/social/primereact-preview.jpg"></meta>
+                <meta property="og:ttl" content="604800"></meta>
+                <link rel="icon" href="https://primefaces.org/cdn/primereact/images/favicon.ico" type="image/x-icon"></link>  
             </Head>
             <div className="landing-intro">
                 <HeaderSection dark={props.dark} onToggleColorScheme={toggleColorScheme} />
