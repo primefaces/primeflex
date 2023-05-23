c0aa95f9bac4887d780440e3851b822393d12bd0
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-05-23 17:23:19 +0300

    dark ui adjustments

--- components/layout/topbar.js
+++ components/layout/topbar.js
@@ -1,8 +1,8 @@
 import { DocSearch } from '@docsearch/react';
-import { useEffect, useRef } from 'react';
-import pkg from '../../package.json';
 import { StyleClass } from 'primereact/styleclass';
 import { classNames } from 'primereact/utils';
+import { useEffect, useRef } from 'react';
+import pkg from '../../package.json';
 
 export default function Topbar(props) {
     const colorSchemeIcon = classNames('pi', { 'pi-sun': props.dark, 'pi-moon': !props.dark });
@@ -55,7 +55,7 @@ export default function Topbar(props) {
     return (
         <div ref={containerElement} className="layout-topbar">
             <div className="layout-topbar-inner">
-                <button type="button" className="link-button menu-button" onClick={onMenuButtonClick} aria-haspopup aria-label="Menu">
+                <button type="button" className="link-button menu-button" onClick={onMenuButtonClick} aria-haspopup aria-label="Menu" style={{ borderColor: 'var(--menu-border-color)' }}>
                     <i className="pi pi-bars"></i>
                 </button>
                 <DocSearch appId="TLMMWZFW0F" apiKey="e6191f9edfded2e69492f3f3b315e237" indexName="primeflex" container="" debug={false} />
@@ -65,7 +65,8 @@ export default function Topbar(props) {
                     <li>
                         <a
                             href="https://github.com/primefaces/primeflex"
-                            className="flex link-button border-1 border-solid w-2rem h-2rem surface-border border-round surface-card align-items-center justify-content-center transition-all transition-duration-300 hover:border-primary"
+                            className="flex link-button border-1 border-solid w-2rem h-2rem border-round surface-card align-items-center justify-content-center transition-all transition-duration-300 hover:border-primary"
+                            style={{ borderColor: 'var(--menu-border-color)' }}
                         >
                             <i className="pi pi-github text-700"></i>
                         </a>
@@ -73,15 +74,17 @@ export default function Topbar(props) {
                     <li>
                         <a
                             href="https://discord.gg/gzKFYnpmCY"
-                            className="flex link-button border-1 border-solid w-2rem h-2rem surface-border border-round surface-card align-items-center justify-content-center transition-all transition-duration-300 hover:border-primary"
+                            className="flex link-button border-1 border-solid w-2rem h-2rem  border-round surface-card align-items-center justify-content-center transition-all transition-duration-300 hover:border-primary"
+                            style={{ borderColor: 'var(--menu-border-color)' }}
                         >
                             <i className="pi pi-discord text-700"></i>
                         </a>
                     </li>
                     <li>
                         <a
-                            className="flex link-button border-1 border-solid w-2rem h-2rem surface-border border-round surface-card align-items-center justify-content-center transition-all transition-duration-300 hover:border-primary"
+                            className="flex link-button border-1 border-solid w-2rem h-2rem  border-round surface-card align-items-center justify-content-center transition-all transition-duration-300 hover:border-primary"
                             onClick={changeColorScheme}
+                            style={{ borderColor: 'var(--menu-border-color)' }}
                         >
                             <i className={classNames('text-700', colorSchemeIcon)}></i>
                         </a>
@@ -92,7 +95,8 @@ export default function Topbar(props) {
                             <button
                                 ref={versionsRef}
                                 type="button"
-                                className="link-button flex align-items-center surface-card h-2rem px-3 hover:surface-hover border-1 border-solid surface-border transition-all transition-duration-300 hover:border-primary"
+                                className="link-button flex align-items-center surface-card h-2rem px-3 hover:surface-hover border-1 border-solid transition-all transition-duration-300 hover:border-primary"
+                                style={{ borderColor: 'var(--menu-border-color)' }}
                             >
                                 <span className="text-900">{versions && versions.length ? versions[0].version : ''}</span>
                                 <span className="ml-2 pi pi-angle-down text-600"></span>
