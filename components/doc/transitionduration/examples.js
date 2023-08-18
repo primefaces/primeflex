580ea12e77d6761ff479b602bcae1c26642e85f8
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-08-18 10:08:02 +0300

    fixed [Showcase] Transition Examples Broken #164

--- components/doc/transitionduration/examples.js
+++ components/doc/transitionduration/examples.js
@@ -3,15 +3,15 @@ import { DocSectionText } from '../common/docsectiontext';
 
 export function ExamplesDoc(props) {
     const code = `<div class="flex flex-wrap align-items-center justify-content-center">
-    <div class="transition-colors transition-duration-100 bg-primary hover:bg-primary hover:text-gray-900
+    <div class="transition-colors transition-duration-100 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
-    <div class="transition-colors transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+    <div class="transition-colors transition-duration-500 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
-    <div class="transition-colors transition-duration-1000 bg-primary hover:bg-primary hover:text-gray-900
+    <div class="transition-colors transition-duration-1000 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
@@ -24,19 +24,19 @@ export function ExamplesDoc(props) {
             <div class="card">
                 <div class="flex flex-wrap align-items-center justify-content-center">
                     <div
-                        class="transition-colors transition-duration-100 bg-primary hover:bg-primary hover:text-gray-900
+                        class="transition-colors transition-duration-100 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
                     </div>
                     <div
-                        class="transition-colors transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+                        class="transition-colors transition-duration-500 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
                     </div>
                     <div
-                        class="transition-colors transition-duration-1000 bg-primary hover:bg-primary hover:text-gray-900
+                        class="transition-colors transition-duration-1000 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
--- components/doc/transitiontimingfunction/examples.js
+++ components/doc/transitiontimingfunction/examples.js
@@ -3,19 +3,19 @@ import { DocSectionText } from '../common/docsectiontext';
 
 export function ExamplesDoc(props) {
     const code = `<div class="flex flex-wrap align-items-center justify-content-center">
-    <div lass="transition-linear transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+    <div lass="transition-linear transition-duration-500 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
-    <div class="transition-ease-in transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+    <div class="transition-ease-in transition-duration-500 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
-    <div class="transition-ease-out transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+    <div class="transition-ease-out transition-duration-500 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
-    <div class="transition-ease-in-out transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+    <div class="transition-ease-in-out transition-duration-500 bg-primary hover:bg-primary-700
         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3">
         Hover me
     </div>
@@ -28,25 +28,25 @@ export function ExamplesDoc(props) {
             <div className="card">
                 <div className="flex flex-wrap align-items-center justify-content-center">
                     <div
-                        className="transition-linear transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+                        className="transition-linear transition-duration-500 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
                     </div>
                     <div
-                        className="transition-ease-in transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+                        className="transition-ease-in transition-duration-500 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
                     </div>
                     <div
-                        className="transition-ease-out transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+                        className="transition-ease-out transition-duration-500 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
                     </div>
                     <div
-                        className="transition-ease-in-out transition-duration-500 bg-primary hover:bg-primary hover:text-gray-900
+                        className="transition-ease-in-out transition-duration-500 bg-primary hover:bg-primary-700
                         flex align-items-center justify-content-center font-bold border-round cursor-pointer m-2 px-5 py-3"
                     >
                         Hover me
