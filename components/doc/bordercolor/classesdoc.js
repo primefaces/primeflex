64753b6b685956f8da2526837f575ea8e136feac
VivianVenter <49275204+VivianVenter@users.noreply.github.com>
2023-09-04 15:16:30 +0200

    Update classesdoc.js
    Adjusting the class name in the div to reflect the correct color in the example for border-100

--- components/doc/bordercolor/classesdoc.js
+++ components/doc/bordercolor/classesdoc.js
@@ -60,7 +60,7 @@ export function ClassesDoc(props) {
                             <td>border-100</td>
                             <td>border-color: var(--surface-100);</td>
                             <td>
-                                <div className="border-200 border-2 shadow-1" style={{ width: '50px', height: '20px' }}></div>
+                                <div className="border-100 border-2 shadow-1" style={{ width: '50px', height: '20px' }}></div>
                             </td>
                         </tr>
                         <tr>
