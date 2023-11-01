d8f255d8027153f4aae14a0bde50a4bd09067761
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-11-01 09:31:53 +0300

    color class generation issue fixed

--- components/doc/textcolor/examples.js
+++ components/doc/textcolor/examples.js
@@ -14,7 +14,7 @@ export function ExamplesDoc(props) {
             <DocSectionText {...props}></DocSectionText>
             <div className="card">
                 <div className="flex flex-wrap md:justify-content-between justify-content-center">
-                    <div className="text-500 w-12rem h-6rem surface-overlay border-round border-1 border-gray-500 font-bold m-2 flex align-items-center justify-content-center">text-500</div>
+                    <div className="text-500 bg-gradient-to-t from-blue-500 w-12rem h-6rem surface-overlay border-round border-1 border-gray-500 font-bold m-2 flex align-items-center justify-content-center">text-500</div>
                     <div className="text-cyan-500 w-12rem h-6rem surface-overlay border-round border-1 border-gray-500 font-bold m-2 flex align-items-center justify-content-center">text-cyan-500</div>
                     <div className="text-orange-500 w-12rem h-6rem surface-overlay border-round border-1 border-gray-500 font-bold m-2 flex align-items-center justify-content-center">text-orange-500</div>
                 </div>
