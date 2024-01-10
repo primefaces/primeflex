bb3ac59774ee0cef588e0f04be801c4d83904e16
Daniel M <inad9300@gmail.com>
2024-01-10 15:14:55 +0100

    Fix duplicated border-200 doc entry

--- components/doc/bordercolor/classesdoc.js
+++ components/doc/bordercolor/classesdoc.js
@@ -57,10 +57,10 @@ export function ClassesDoc(props) {
                             </td>
                         </tr>
                         <tr>
-                            <td>border-200</td>
+                            <td>border-100</td>
                             <td>border-color: var(--surface-100);</td>
                             <td>
-                                <div className="border-200 border-2 shadow-1" style={{ width: '50px', height: '20px' }}></div>
+                                <div className="border-100 border-2 shadow-1" style={{ width: '50px', height: '20px' }}></div>
                             </td>
                         </tr>
                         <tr>
